c0b9230d1d4a833fd4c87121cfdb7f02f0c4831e
Daniel Versiane <daniel.versiane@gmail.com>
2021-02-06 11:04:53 -0300

    :tada: first commit.

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+  DocumentProps,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from 'next/document'
+
+class MyDocument extends Document<DocumentProps> {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000" />
+
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Title App</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
